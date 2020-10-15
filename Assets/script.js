3cdd33c47038c13e56344938408fe59089648f37
Jambi 46.2 <69945554+rayna-v@users.noreply.github.com>
2020-10-14 20:46:00 -0600

    fixed broken link to script.js, moved script from HTML to JS, attempted to use .each to get function to cycle through questions array

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,46 @@
-$(document).ready(function () {
 
+
+var questions = [$('#q1'), $('#q2'), $('#q3'), $('#q4'), $('#q5'), $('#q6'), $('#q7'), $('#q8'), $('#q9'), $('#q10')]
+
+// console.log(questions.value('#q2'));
+
+$(document).ready(function () {
+    //hides questions until 'Begin Quiz' button is clicked
+    $(".questions").hide();
+    //  listens for 'click' on 'btn-start'/Begin Quiz and then executes the following code block
     $("#btn-start").on("click", function () {
+        //call function to start timer
+
+        //hides 'quiz-start' card
         $("#quiz-start").hide();
+        $('#q1').show();
+
+        var oldQ = [];
+        var currentQ = [];
+
+        $.each(questions, function (i) {
+            // questions[i].show();
+            $('#true').on("click", function () {
+                oldQ = questions[i].hide();
+                currentQ = questions[i++].show();
+            })
+            return oldQ;
+            return currentQ;
+        })
 
     })
 
 
+    // for (let i = 0; i < questions.length; i++) {
 
-
-
-
+    //     console.log(questions[i]);
+    //     // var showQ = questions[i].show();
+    //     $('#true').on("click", function () {
+    //         $('#q1').hide();
+    //         oldQ = questions[i].hide();
+    //         currentQ = questions[i++].show();
+    //     })
+    //     return oldQ;
+    //     return currentQ;
+    // }
 })
